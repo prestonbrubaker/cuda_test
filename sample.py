6a5a026863f367d83496e2005abf897cfe6c5364
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-18 22:16:19 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,6 +5,10 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import torchvision.models as models
 
+model = models.vgg16() # we do not specify ``weights``, i.e. create untrained model
+model.load_state_dict(torch.load('model_weights.pth'))
+model.eval()
+
 training_data = datasets.FashionMNIST(
     root="data",
     train=True,
