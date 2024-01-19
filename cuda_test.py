4bbf9e02da544035d347261c79bfb194446f05e9
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-18 20:26:10 -0600

    Update cuda_test.py

--- cuda_test.py
+++ cuda_test.py
@@ -24,3 +24,6 @@ if torch.cuda.is_available():
     tensor.to("cuda")
 
 print(f"Device tensor is stored on: {tensor.device}")   # Display if the tensor is stored on the cpu or gpu
+
+print(torch.cuda.is_available())  # Should return True if CUDA is properly installed
+print(torch.cuda.get_device_name(0))  # Should return the name of your CUDA GPU
