c60169ce4d0b29d4b60b59babff2ff893b2c9c97
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-20 10:39:53 -0600

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ def is_in_mandelbrot(c, max_iter=500):
     return 1  # In the Mandelbrot set
 
 # Creating a dataset
-data_size = 1000000  # Number of data points
+data_size = 10000  # Number of data points
 X = np.random.uniform(-2, 2, (data_size, 2))  # Generate random pairs
 y = np.array([is_in_mandelbrot(complex(x[0], x[1])) for x in X])
 
