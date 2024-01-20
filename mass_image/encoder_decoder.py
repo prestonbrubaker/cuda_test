c49dd9ca0c3d9b7b151e4a69c1cd34968e549f46
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-20 16:45:37 -0600

    Update encoder_decoder.py

--- mass_image/encoder_decoder.py
+++ mass_image/encoder_decoder.py
@@ -65,7 +65,7 @@ criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # Train the model
-num_epochs = 50
+num_epochs = 5000
 for epoch in range(num_epochs):
     for data in dataloader:
         img = data.to(device)
