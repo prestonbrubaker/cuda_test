d0e164a4c6e4eecf603f510de6297a6cb58c0d9b
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-19 23:05:29 -0600

    Update train.py

--- train.py
+++ train.py
@@ -52,7 +52,7 @@ model = MandelbrotNet().to(device)
 
 # Loss and optimizer
 criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
 
 # Move data to GPU
 X = X.to(device)
