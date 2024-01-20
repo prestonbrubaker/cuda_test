45dfb217d787a409e03ecca8370cb88f3892fe18
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-19 22:10:33 -0600

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ def is_in_mandelbrot(c, max_iter=500):
     return 1  # In the Mandelbrot set
 
 # Creating a dataset
-data_size = 10000  # Number of data points
+data_size = 100000  # Number of data points
 X = np.random.uniform(-2, 2, (data_size, 2))  # Generate random pairs
 y = np.array([is_in_mandelbrot(complex(x[0], x[1])) for x in X])
 
@@ -51,7 +51,7 @@ X = X.to(device)
 y = y.to(device)
 
 # Training
-epochs = 10000
+epochs = 1000000
 for epoch in range(epochs):
     optimizer.zero_grad()
     outputs = model(X)
