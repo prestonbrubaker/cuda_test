e25559c6b1c677acd534edd3345696bf1e75e811
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-20 23:27:23 -0600

    Update encoder_decoder.py

--- mass_image/encoder_decoder.py
+++ mass_image/encoder_decoder.py
@@ -114,8 +114,8 @@ model = Autoencoder().to(device)
 
 # Loss and optimizer
 criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-#optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.01)
+#optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.2, momentum=0.9)
 
 # Train the model
 num_epochs = 10000
