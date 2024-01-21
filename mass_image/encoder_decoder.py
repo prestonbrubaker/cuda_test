1c93ce4971628709dc8532bef0cfb4a6ed5a3860
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-21 10:43:36 -0600

    Update encoder_decoder.py

--- mass_image/encoder_decoder.py
+++ mass_image/encoder_decoder.py
@@ -68,7 +68,7 @@ transform = transforms.Compose([
 # Load dataset
 dataset = MandelbrotDataset(folder_path='semi_synthetic_photos', transform=transform)
 #dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=True)  # Choose an appropriate batch size
-dataloader = DataLoader(dataset, batch_size=200, shuffle=True)  # Choose an appropriate batch size
+dataloader = DataLoader(dataset, batch_size=400, shuffle=True)  # Choose an appropriate batch size
 
 # Instantiate model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
