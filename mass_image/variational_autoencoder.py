27914258763c6ec2e171598ec2e262891e08f515
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-21 16:17:35 -0600

    Update variational_autoencoder.py

--- mass_image/variational_autoencoder.py
+++ mass_image/variational_autoencoder.py
@@ -130,8 +130,7 @@ model = VariationalAutoencoder().to(device)
 
 # Loss and optimizer
 # For VAE, use the custom loss function that includes both BCE and KLD
-#optimizer = optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, amsgrad=True)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.05, momentum=0.9, eps=1e-8)
+optimizer = optim.Adam(model.parameters(), lr=0.05, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, amsgrad=True)
 
 # Train the model
 num_epochs = 100000
