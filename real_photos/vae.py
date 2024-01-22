0883ce721db6f93019276724c49970075770174c
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-21 22:22:27 -0600

    Update vae.py

--- real_photos/vae.py
+++ real_photos/vae.py
@@ -7,7 +7,7 @@ from PIL import Image
 import os
 
 # Model Parameters
-latent_dim = 100  # Example latent space dimension
+latent_dim = 10  # Example latent space dimension
 LATENT_DIM = latent_dim
 
 class VariationalAutoencoder(nn.Module):
