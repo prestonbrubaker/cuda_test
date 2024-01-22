1d75644e04288d00d4fee671c2122c9dee744000
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-21 19:42:08 -0600

    Update variational_autoencoder.py

--- mass_image/variational_autoencoder.py
+++ mass_image/variational_autoencoder.py
@@ -118,7 +118,7 @@ if USE_PREPROCESSED_DATASET:
 else:
     dataset = MandelbrotDataset(folder_path='semi_synthetic_photos', transform=transform)
 
-dataloader = DataLoader(dataset, batch_size=600, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=60, shuffle=True)
 
 
 # Instantiate VAE model
